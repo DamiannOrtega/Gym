37fe21547dd0485bf9131a1fee918180955ed4ee
Alanggj <alangalli100@gmail.com>
2025-04-24 19:54:16 -0600

    Clases destacadas

--- Gym/src/app/pages/home/home.component.ts
+++ Gym/src/app/pages/home/home.component.ts
@@ -11,9 +11,9 @@ import { RouterModule } from '@angular/router';
 })
 export class HomeComponent {
   clasesDestacadas = [
-    { id: 1, nombre: 'Zumba', icono: 'fas fa-music', descripcion: 'Cardio con ritmo latino para todos los niveles.', image: '/assets/img/gym1.jpg' },
-    { id: 2, nombre: 'CrossFit', icono: 'fas fa-dumbbell', descripcion: 'Entrenamiento funcional de alta intensidad.', image: '/assets/img/gym1.jpg' },
-    { id: 3, nombre: 'Yoga', icono: 'fas fa-spa', descripcion: 'Conecta cuerpo y mente con sesiones de yoga guiadas.', image: '/assets/img/gym1.jpg' }
+    { id: 1, nombre: 'Zumba', icono: 'fas fa-music', descripcion: 'Cardio con ritmo latino para todos los niveles.', image: '/assets/img/zumba.avif' },
+    { id: 2, nombre: 'CrossFit', icono: 'fas fa-dumbbell', descripcion: 'Entrenamiento funcional de alta intensidad.', image: '/assets/img/crossfit.jpg' },
+    { id: 3, nombre: 'Yoga', icono: 'fas fa-spa', descripcion: 'Conecta cuerpo y mente con sesiones de yoga guiadas.', image: '/assets/img/yoga.jpg' }
   ];
   
 
