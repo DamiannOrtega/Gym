cec8ab0c66701f0b9e133b4ae475a7732dde9847
Miranother <115019501+Miranother@users.noreply.github.com>
2025-04-25 16:42:54 -0600

    buscador como en la tarea

--- Gym/src/app/pages/clases/clases.component.ts
+++ Gym/src/app/pages/clases/clases.component.ts
@@ -3,25 +3,39 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Clase } from '../../models/clase.model';
 import { ClasesService } from '../../services/clases.service';
+import { BuscadorComponent } from '../../shared/buscador/buscador.component';
 
 @Component({
   selector: 'app-clases',
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule,CommonModule,BuscadorComponent],
   templateUrl: './clases.component.html',
  styleUrls: ['./clases.component.css']
 
 })
 export class ClasesComponent {
     clasesDestacadas: Clase[] = [];
-
+    clasesOriginales: Clase[] = []; 
     constructor(private clasesService: ClasesService) {}
 
     ngOnInit(): void {
       this.clasesService.getClases().subscribe((data) => {
         this.clasesDestacadas = data;
+        this.clasesOriginales = data; 
       });
+    }
+  
+    filtrarClases(termino: string) {
+      if (!termino.trim()) {
+        this.clasesDestacadas = this.clasesOriginales; 
+      } else {
+        const terminoLower = termino.toLowerCase();
+        this.clasesDestacadas = this.clasesOriginales.filter(clase =>
+          clase.nombre.toLowerCase().includes(terminoLower)
+          
+        );
+      }
+    }
 
   
 
-  }
 }
--- Gym/src/app/shared/buscador/buscador.component.ts
+++ Gym/src/app/shared/buscador/buscador.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-buscador',
+  standalone: true,
   imports: [],
   templateUrl: './buscador.component.html',
   styleUrl: './buscador.component.css'
 })
 export class BuscadorComponent {
+  @Output() buscar = new EventEmitter<string>();
+
+  onInput(event: Event) {
+    const texto = (event.target as HTMLInputElement).value;
+    this.buscar.emit(texto);
+  }
 
 }
