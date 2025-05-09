44c99e13adf29cc63dbc54f26dac070e33d12213
Miranother <115019501+Miranother@users.noreply.github.com>
2025-05-09 10:45:04 -0600

    fotos

--- Gym/src/app/shared/my-contacto/my-contacto.component.ts
+++ Gym/src/app/shared/my-contacto/my-contacto.component.ts
@@ -15,10 +15,10 @@ export class MyContactoComponent {
       titulo: 'Carlos Enrique Blanco Ortiz', 
       textoFrente: 'Entrenador Profesional', 
       trasera: 'Mis redes!', 
-      imagen: 'assets/img/Shadi.jpg',
+      imagen: 'assets/img/enrique.jpg',
       icons: [
-        { class: 'fa-brands fa-instagram', url: 'https://www.instagram.com' },
-        { class: 'fa-brands fa-twitter',url: 'https://www.twitter.com' },
+        { class: 'fa-brands fa-instagram', url: 'https://www.instagram.com/c_enrique_blanco?igsh=cWN4amZ3NzZhN2Uz' },
+        { class: 'fa-brands fa-twitter',url: 'https://x.com/miranother?t=I-pR2Y8LQjzFQR7JTjzjJQ&s=09' },
         { class: 'fa-brands fa-whatsapp', url: 'https://wa.me/1234567890' }
       ]
     },
@@ -27,9 +27,9 @@ export class MyContactoComponent {
       titulo: 'Alan Gael Gallardo Jimenez', 
       textoFrente: 'Entrenador Profesional', 
       trasera: 'Siguemeeee!', 
-      imagen: 'assets/img/carlitos.jpg',
+      imagen: 'assets/img/alan.jpg',
       icons: [
-        { class: 'fa-brands fa-instagram', url: 'https://www.instagram.com' },
+        { class: 'fa-brands fa-instagram', url: 'https://www.instagram.com/alan.gael.37?igsh=MTc3cXB1a2FlY2ZoNw%3D%3D&utm_source=qr' },
         { class: 'fa-brands fa-twitter',url: 'https://www.twitter.com' },
         { class: 'fa-brands fa-whatsapp', url: 'https://wa.me/1234567890' }
       ]
@@ -41,8 +41,8 @@ export class MyContactoComponent {
       trasera: 'Aqui me encuentras...', 
       imagen: 'assets/img/zeling.jpg',
       icons: [
-        { class: 'fa-brands fa-instagram', url: 'https://www.instagram.com' },
-        { class: 'fa-brands fa-twitter',url: 'https://www.twitter.com' },
+        { class: 'fa-brands fa-instagram', url: 'https://www.instagram.com/damiannortega?igsh=cDN5ZnFoYjJhZXRj' },
+        { class: 'fa-brands fa-twitter',url: 'https://x.com/Pushezz?t=ldCh7fwYbCVt8S8N8IcVVg&s=09' },
         { class: 'fa-brands fa-whatsapp', url: 'https://wa.me/1234567890' }
       ]
     }
