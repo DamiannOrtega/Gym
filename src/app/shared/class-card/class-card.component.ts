efece8c6859005393fc378f2ebc9fbe63453089e
DamiannOrtega <150359248+DamiannOrtega@users.noreply.github.com>
2025-05-11 21:18:59 -0600

    q

--- src/app/shared/class-card/class-card.component.ts
+++ src/app/shared/class-card/class-card.component.ts
@@ -10,9 +10,9 @@ import { RouterModule } from '@angular/router';
 })
 export class ClassCardComponent {
   clasesDestacadas = [
-    { id: 1, nombre: 'Zumba', icono: 'fas fa-music', descripcion: 'Cardio con ritmo latino para todos los niveles.', image: '/assets/img/zumba.avif' },
-    { id: 2, nombre: 'CrossFit', icono: 'fas fa-dumbbell', descripcion: 'Entrenamiento funcional de alta intensidad.', image: '/assets/img/crossfit.jpg' },
-    { id: 3, nombre: 'Yoga', icono: 'fas fa-spa', descripcion: 'Conecta cuerpo y mente con sesiones de yoga guiadas.', image: '/assets/img/yoga.jpg' }
+    { id: 1, nombre: 'Zumba', icono: 'fas fa-music', descripcion: 'Cardio con ritmo latino para todos los niveles.', image: './assets/img/zumba.avif' },
+    { id: 2, nombre: 'CrossFit', icono: 'fas fa-dumbbell', descripcion: 'Entrenamiento funcional de alta intensidad.', image: './assets/img/crossfit.jpg' },
+    { id: 3, nombre: 'Yoga', icono: 'fas fa-spa', descripcion: 'Conecta cuerpo y mente con sesiones de yoga guiadas.', image: './assets/img/yoga.jpg' }
   ];
   
 
