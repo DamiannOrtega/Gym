530a67322fd68174242e4272c8fef6702e4176ab
Alanggj <alangalli100@gmail.com>
2025-05-30 20:08:43 -0600

    Loading funcional en todas las rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, computed, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
+import { LoadingService } from './services/loading.service';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +14,39 @@ import { LoaderComponent } from './components/loader/loader.component';
 })
 export class AppComponent {
   title = 'Gym';
-  loading = true;
 
-  constructor() {
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-        this.loading = false;
-      }, 2000);
+  loading;
+
+  constructor(private loadingService: LoadingService, private router: Router) {
+
+    this.loading = this.loadingService.loading;
+
+    // Suscripción a eventos de router
+    this.router.events.subscribe(event => {
+      if (event instanceof RouteConfigLoadStart) {
+        this.loadingService.show();
+      } else if (event instanceof RouteConfigLoadEnd) {
+        this.loadingService.hide();
+      }
+
+      if (event instanceof NavigationStart) {
+        this.loadingService.show();
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.loadingService.hide();
+      }
     });
+  }
+
+  ngAfterViewInit() {
+    // Quita el loader estático de index.html una vez Angular cargue el componente
+    const initialLoader = document.getElementById('initial-loader');
+    if (initialLoader) {
+      initialLoader.style.display = 'none';
+    }
   }
 }
 
