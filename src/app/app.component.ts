e9cce6ef7ec25dc8d9037ce59091cd3e97a18e30
Alanggj <alangalli100@gmail.com>
2025-05-30 19:07:57 -0600

    Loading solo al inicio de la pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Gym';
+  loading = true;
+
+  constructor() {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000);
+    });
+  }
 }
+
