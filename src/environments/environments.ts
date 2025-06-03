ac3f31a8aaf7607d84e72a8eb4521b7de6d32ebe
Miranother <115019501+Miranother@users.noreply.github.com>
2025-06-02 19:34:39 -0600

    Integracion de firebase y firestore en el proyecto

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyBqwA23u4621vIwKxqiFOIYMZUbjNsJVCk",
+      authDomain: "gymweb-2025.firebaseapp.com",
+      projectId: "gymweb-2025",
+      storageBucket: "gymweb-2025.appspot.com",
+      messagingSenderId: "58715135721",
+      appId: "1:58715135721:web:1f9fcecc9e114c980bda78"
+    }
+  };
+  
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environments';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule)
+  ],
+
+});
