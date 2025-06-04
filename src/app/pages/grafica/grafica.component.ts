0e62c10309987fd4fe965d235f8e4fffbdb78a1d
Miranother <carlosblancoortiz16442@gmail.com>
2025-06-04 09:29:08 -0600

    Creacion e implementacion del nuevo componente de graficas

---
+++ src/app/pages/grafica/grafica.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica',
+  imports: [],
+  templateUrl: './grafica.component.html',
+  styleUrl: './grafica.component.css'
+})
+export class GraficaComponent {
+
+}
