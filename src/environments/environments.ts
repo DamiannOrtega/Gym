39072d2be1ca88fb70e822cc6200793138e08e4c
Miranother <carlosblancoortiz16442@gmail.com>
2025-06-14 12:07:45 -0600

    base de repuesto

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,12 +1,27 @@
-export const environment = {
+//Base 1
+// export const environment = {
+//     production: false,
+//     firebaseConfig: {
+//       apiKey: "AIzaSyBqwA23u4621vIwKxqifOIYMZUbjNSJVCk",
+//       authDomain: "gymweb-2025.firebaseapp.com",
+//       projectId: "gymweb-2025",
+//       storageBucket: "gymweb-2025.firebasestorage.app",
+//       messagingSenderId: "58715135721",
+//       appId: "1:58715135721:web:1f9fcecc9e114c980bda78"
+//     }
+//   };
+
+//ctrl k+ctrl u descomenta
+  
+//Base 2
+  export const environment = {
     production: false,
     firebaseConfig: {
-      apiKey: "AIzaSyBqwA23u4621vIwKxqifOIYMZUbjNSJVCk",
-      authDomain: "gymweb-2025.firebaseapp.com",
-      projectId: "gymweb-2025",
-      storageBucket: "gymweb-2025.firebasestorage.app",
-      messagingSenderId: "58715135721",
-      appId: "1:58715135721:web:1f9fcecc9e114c980bda78"
+      apiKey: "AIzaSyBU_bUd_uTAMExG76PlJn6mtMj7n0bNVSI",
+      authDomain: "gimfitzonerepuesto1.firebaseapp.com",
+      projectId: "gimfitzonerepuesto1",
+      storageBucket: "gimfitzonerepuesto1.firebasestorage.app",
+      messagingSenderId: "426992264515",
+      appId: "1:426992264515:web:ecc4067deb06f1d8463bf6"
     }
   };
-  
